1476d45afba1893ffd60db4d1da76f9057cadaac
Muhammad-Hunain <hunain29.mem@gmail.com>
2022-08-23 00:55:00 +0500

    NODE CRUD

---
+++ NodeJS CRUD/app.js
@@ -0,0 +1,37 @@
+// const fs = require('fs')
+// const path= require('path')
+// const dirpath = path.join(__dirname,'CRUD')
+// To make a file and Also write the Text
+// fs.writeFileSync(`${dirpath}/data.txt`,"My name is Hunain");
+
+// const filePath = `${dirpath}/Data.txt`
+
+
+// To Write a Text in File(Create)
+
+
+// fs.writeFileSync(filePath,"My Name SI muhammad Hunain");
+
+// To Read a Text in File(READ)
+
+// fs.readFile(filePath,'utf8',(err,item)=>{
+//         console.log(item)
+// })
+
+// To Update a Text in the File(Update)
+
+// fs.appendFile(filePath," and I am a Full Stack Developer",(err)=>{
+//     if(!err) console.log("Updated File")
+// })
+
+// To Rename File(RENAME)
+
+// fs.rename(filePath,`${dirpath}/BigData.txt`,(err)=>{
+//     if(!err) console.log("File Name IS Updated")
+// })
+
+// To Delete File(DELETE)
+
+// fs.unlink(`${dirpath}/BigData.txt`,(err)=>{
+//     if(!err) console.log("file Deleted")
+// })
