6cad42d42328d237970ddd0b2c7f0829b32e5777
Muhammad-Hunain <hunain29.mem@gmail.com>
2022-08-26 03:28:58 +0500

    Midlware

---
+++ Middleware/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+const reqFilter=(req,resp,next)=>{
+        if(!req.query.age){
+            resp.send("please provide a age")
+        }
+        else if(req.query.age < 18){
+            resp.send("You can't acess this page")
+
+        }
+        else{
+            next()
+        }
+}
+ app.use(reqFilter);
+
+app.get('/',(_,resp)=>{
+    resp.send("Welcome ! to Home page")
+})
+
+app.get('/about',(_,resp)=>{
+    resp.send("Welcome ! to about page")
+})
+
+app.listen(5000)
